53887d327ac0f7888337e9f72f08758b2ae47fed
Prorog <proroog@gmail.com>
2025-03-13 17:18:03 +0100

    #1 - Konto-W-Banku
    [+] `Konto` Class
    [+] Main tests for `Konto` Class

---
+++ Konto-w-banku/Bank/Bank/Bank.cs
@@ -0,0 +1,63 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        private string klient;
+        public string Klient {
+            get => klient;
+            private set => klient = value.Trim() != "" ? value.Trim() : throw new ArgumentException("Nazwa klienta nie moze byc pusta!"); }
+        private decimal bilans;
+        public decimal Bilans {
+            get => bilans; 
+            private set => bilans = value > 0 ? value : 0; }
+        private bool zablokowane;
+        public bool Zablokowane { get => zablokowane; private set => zablokowane = false; }
+
+        //private Konto() { }
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            Klient = klient;
+            Bilans = bilansNaStart;
+        }
+
+        public string Statystyki()
+        {
+            var status = zablokowane ? "Zablokowane" : "Odblokowane";
+            return $"Nazwa:\t\t{klient}\n" +
+                   $"Bilans:\t\t{bilans}\n" +
+                   $"Status konta:\t{status}";
+        }
+
+        public virtual void Wplata(decimal kwota)
+        {
+            if (zablokowane) throw new ArgumentException("Konto jest zablokowane!");
+
+            if (kwota > 0)
+            {
+                bilans += kwota;
+            }
+            else
+            {
+                throw new ArgumentException("Nieprawidlowa kwota wplaty!!");
+            }
+        }
+        public virtual void Wyplata(decimal kwota)
+        {
+            if (zablokowane) throw new ArgumentException("Konto jest zablokowane!");
+
+            if (kwota > 0 && kwota <= bilans)
+            {
+                bilans += kwota;
+            }
+            else
+            {
+                throw new ArgumentException("Nieprawidlowa kwota wyplaty!!");
+            }
+        }
+
+        public void BlokujKonto() => zablokowane = true;
+        public void OdblokujKonto() => zablokowane = false;
+    }
+
+    
+}
---
+++ Konto-w-banku/Bank/KontoTests/UnitTest1.cs
@@ -0,0 +1,345 @@
+using Bank;
+
+namespace KontoTests
+{
+    [TestClass]
+    public class TEST_Konto_Create
+    {
+        [TestMethod]
+        public void KontoCreate()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                Assert.IsTrue(k1 != null);
+            }
+            catch (ArgumentException ex)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void KontoCreate_NameIsNull()
+        {
+            string klient = "";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                Assert.IsTrue(k1 == null, "Wystapil blad! Utworzono klienta bez nazwy!");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Nazwa klienta nie moze byc pusta!"), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoCreate_NegativeStartingMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = -100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                Assert.IsTrue(k1.Bilans >= 0, "Wystapil blad! Utworzono startowy bilans na minusie!");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+    }
+
+    [TestClass]
+    public class TEST_Konto_Functions
+    {
+        [TestMethod]
+        public void KontoStatistics_UnlockedAccount()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                k1.OdblokujKonto();
+
+                string res = k1.Statystyki();
+                string pred = $"Nazwa:\t\t{klient}\nBilans:\t\t{bilans}\nStatus konta:\tOdblokowane";
+
+                Assert.IsTrue(res.Equals(pred), "Wystapil blad! Sprawdz funkcje Statystyki");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoStatistics_LockedAccount()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                k1.BlokujKonto();
+
+                string res = k1.Statystyki();
+                string pred = $"Nazwa:\t\t{klient}\nBilans:\t\t{bilans}\nStatus konta:\tZablokowane";
+
+                Assert.IsTrue(res.Equals(pred), "Wystapil blad! Sprawdz funkcje Statystyki");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+    }
+
+    [TestClass]
+    public class TEST_Konto_Methods
+    {
+        [TestMethod]
+        public void Konto_UnlockAccount()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                k1.OdblokujKonto();
+                Assert.IsTrue(!k1.Zablokowane, "Wystapil blad! Konto dalej jest zablokowane!");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void Konto_LockAccount()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                k1.BlokujKonto();
+                Assert.IsTrue(k1.Zablokowane, "Wystapil blad! Konto dalej jest odblokowane!");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWplata_UnlockedAccount_ValidInput()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                decimal kwota = 100M;
+                k1.Wplata(kwota);
+                Assert.IsTrue(k1.Bilans == (bilans + kwota), "Wystapil blad! Bilans konta sie nie zgadza!");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWplata_LockedAccount_ValidInput()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                decimal kwota = 100M;
+                k1.Wplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Konto jest zablokowane!"), "Wystapil blad! Sprawdz dokladnie metode Wplata()");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWyplata_UnlockedAccount_ValidInput()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                decimal kwota = 100M;
+                k1.Wyplata(kwota);
+                Assert.IsTrue(k1.Bilans == (bilans + kwota), "Wystapil blad! Bilans konta sie nie zgadza!");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains(""), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWyplata_LockedAccount_ValidInput()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                decimal kwota = 100M;
+                k1.Wyplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Konto jest zablokowane!"), "Wystapil blad! Sprawdz dokladnie metode Wyplata()");
+            }
+        }
+
+        /* */
+
+        [TestMethod]
+        public void KontoWplata_UnlockedAccount_InvalidInput_NegativeMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                decimal kwota = -100M;
+                k1.Wplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Nieprawidlowa kwota wplaty!!"), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWplata_LockedAccount_InvalidInput_NegativeMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = -100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                decimal kwota = 100M;
+                k1.Wplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Konto jest zablokowane!"), "Wystapil blad! Sprawdz dokladnie metode Wplata()");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWyplata_UnlockedAccount_InvalidInput_NegativeMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                decimal kwota = -100M;
+                k1.Wplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Nieprawidlowa kwota wplaty!!"), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWyplata_LockedAccount_InvalidInput_NegativeMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = -100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                decimal kwota = 100M;
+                k1.Wplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Konto jest zablokowane!"), "Wystapil blad! Sprawdz dokladnie metode Wplata()");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWyplata_UnlockedAccount_InvalidInput_TooMuchMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.OdblokujKonto();
+                decimal kwota = 200M;
+                k1.Wyplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Nieprawidlowa kwota wyplaty!!"), "Wystapil blad!");
+            }
+        }
+
+        [TestMethod]
+        public void KontoWyplata_LockedAccount_InvalidInput_TooMuchMoney()
+        {
+            string klient = "Andrzej";
+            decimal bilans = 100M;
+
+            try
+            {
+                Konto k1 = new(klient, bilans);
+                k1.BlokujKonto();
+                decimal kwota = 200M;
+                k1.Wyplata(kwota);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.ToString().Contains("Konto jest zablokowane!"), "Wystapil blad! Sprawdz dokladnie metode Wyplata()");
+            }
+        }
+    }
+}
