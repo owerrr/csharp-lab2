968a2534c923c31587f844ffc3c6598b0ec5bd40
Prorog <proroog@gmail.com>
2025-03-31 23:36:44 +0200

    Pudelko - 4-13

---
+++ Pudelko/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PudelkoLibrary;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("TEST");
+        Pudelko p1 = new(2.5d, 9.321d, 0.1d, UnitOfMeasure.meter);
+
+        Console.WriteLine(p1.ToString());
+        Console.WriteLine(p1.ToString("cm"));
+        Console.WriteLine(p1.ToString("mm"));
+        Console.WriteLine(p1.ToString("m"));
+        Console.WriteLine($"{p1.Objetosc} m3");
+        Console.WriteLine($"{p1.Pole} m2");
+
+        Pudelko p2 = new(1d, 2.1d, 3.05d, UnitOfMeasure.meter);
+        Pudelko p3 = new(1d, 3.05d, 2.1d, UnitOfMeasure.meter);
+
+        Console.WriteLine(p2 == p3);
+        Console.WriteLine(p2+p3);
+        Console.WriteLine(p2[0]);
+        Console.WriteLine(p2[1]);
+        Console.WriteLine(p2[2]);
+        //Console.WriteLine(p2[3]);
+        Console.WriteLine("FOREACH =");
+        foreach(var item in p2)
+        {
+            Console.WriteLine($"{item}"); 
+        }
+
+        Pudelko p4 = Pudelko.Parse("2.500 m × 9321 mm × 0.100 m");
+    }
+}
--- Pudelko/PudelkoClass/Pudelko.cs
+++ Pudelko/PudelkoClass/Pudelko.cs
@@ -1,4 +1,7 @@
-﻿namespace PudelkoLibrary
+﻿using System.Collections;
+using System.Globalization;
+
+namespace PudelkoLibrary
 {
     public enum UnitOfMeasure
     {
@@ -6,7 +9,7 @@
         centimeter = 100,
         meter = 1
     }
-    public class Pudelko
+    public sealed class Pudelko : IEquatable<Pudelko>, IEnumerable<double>
     {
         //private UnitOfMeasure _unitOfMeasure{get;set;}
         //public UnitOfMeasure unitOfMeasure { get => _unitOfMeasure; set => _unitOfMeasure = value; }
@@ -42,6 +45,15 @@
             }
         }
 
+        public double Objetosc
+        {
+            get => Math.Round(A * B * C, 9);
+        }
+        public double Pole
+        {
+            get => Math.Round(2 * A * B + 2 * B * C + 2 * A * C , 6);
+        }
+
         public Pudelko(double? a, double? b, double? c, UnitOfMeasure unitOfMeasure = UnitOfMeasure.meter) 
         {
             int converter = (int)unitOfMeasure;
@@ -71,22 +83,146 @@
         
         public string ToString(string format)
         {
+            if (format != "m" && format != "cm" && format != "mm") throw new ArgumentException("invalid format!");
+
             int converter = (int)UnitOfMeasure.meter;
-            string shortFormat = "m";
-            if (format == "centimeter")
+            double _a = A;
+            double _b = B;
+            double _c = C;
+
+            if (format == "cm")
             {
                 converter = (int)UnitOfMeasure.centimeter;
-                shortFormat = "cm";
+                _a = Math.Round(_a * converter, 1);
+                _b = Math.Round(_b * converter, 1);
+                _c = Math.Round(_c * converter, 1);
             }
-            else if (format == "milimeter")
+            else if (format == "mm")
             {
                 converter = (int)UnitOfMeasure.milimeter;
-                shortFormat = "mm";
+                _a = Math.Floor(_a * converter);
+                _b = Math.Floor(_b * converter);
+                _c = Math.Floor(_c * converter);
             }
-                
 
-            double[] values = { A * converter, B * converter, C * converter };
-            return $"{values[0]} {shortFormat} × {values[1]} {shortFormat} × {values[2]} {shortFormat}";
+            double[] values = { _a, _b, _c };
+
+
+
+            return $"{values[0]} {format} × {values[1]} {format} × {values[2]} {format}";
+        }
+
+        public static bool Equals(Pudelko p1, Pudelko p2)
+        {
+            if (p1 is null || p2 is null) return false;
+
+            return p1.Pole == p2.Pole;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(A, B, C);
+        }
+
+        public bool Equals(Pudelko p)
+        {
+            if (p is null) return false;
+
+            return Pole == p.Pole;
+        }
+
+        public override bool Equals(object? other)
+        {
+            Pudelko p = other as Pudelko;
+            if (p is null) return false;
+            return Equals(p);
+        }
+
+        public static bool operator ==(Pudelko p1, Pudelko p2)
+        {
+            return Equals(p1, p2);
+        }
+
+        public static bool operator !=(Pudelko p1, Pudelko p2)
+        {
+            return Equals(p1, p2);
+        }
+
+        public double this[int idx]
+        {
+            get
+            {
+                switch (idx)
+                {
+                    case 0:
+                        return A;
+                    case 1:
+                        return B;
+                    case 2:
+                        return C;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+        }
+        public IEnumerator<double> GetEnumerator()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public static Pudelko Parse(string input)
+        {
+            string[] values = input.Replace(" ", String.Empty).Split('×');
+            double[] pudelko_abc = new double[3];
+            for(int i = 0; i < 3; i++)
+            {
+                string s_tmp = "";
+                string unit_tmp = "";
+                foreach(char c in values[i])
+                {
+                    if (Char.IsDigit(c) || c == '.' || c == ',')
+                        s_tmp += c;
+                    else
+                        unit_tmp += c;
+                }
+                pudelko_abc[i] = double.Parse(s_tmp, CultureInfo.InvariantCulture);
+                if (unit_tmp == "cm")
+                    pudelko_abc[i] /= (int)UnitOfMeasure.centimeter;
+                else if (unit_tmp == "mm")
+                    pudelko_abc[i] /= (int)UnitOfMeasure.milimeter;
+            }
+            return new Pudelko(pudelko_abc[0], pudelko_abc[1], pudelko_abc[2], UnitOfMeasure.meter);
+        }
+
+        public static Pudelko operator+(Pudelko p1, Pudelko p2)
+        {
+            //double[] data = new double[3];
+
+            //data[0] = p1.A > p2.A ? p1.A : p2.A;
+            //data[1] = p1.B > p2.B ? p1.B : p2.B;
+            //data[2] = p1.C > p2.C ? p1.C : p2.C;
+
+            //return new Pudelko(data[0], data[1], data[2], UnitOfMeasure.meter);
+
+            return new Pudelko(p1.A + p2.A, p1.B + p2.B, p1.C + p2.C, UnitOfMeasure.meter);
+        }
+
+        public static explicit operator double[](Pudelko p)
+        {
+            return [ p.A, p.B, p.C ];
+        }
+
+        public static implicit operator Pudelko(ValueTuple<int,int,int> val)
+        {
+            return new Pudelko(val.Item1 / (int)UnitOfMeasure.milimeter, val.Item2 / (int)UnitOfMeasure.milimeter, val.Item3 / (int)UnitOfMeasure.milimeter);
         }
     }
 }
