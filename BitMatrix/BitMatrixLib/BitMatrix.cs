4637e4a80ec8f375a96ddbb339565640a5d52853
Prorog <proroog@gmail.com>
2025-04-14 13:15:22 +0200

    BitMatrix 8/9 & Readme edits

--- BitMatrix/BitMatrixLib/BitMatrix.cs
+++ BitMatrix/BitMatrixLib/BitMatrix.cs
@@ -2,7 +2,7 @@
 
 namespace BitMatrixLib
 {
-    public class BitMatrix : IEquatable<BitMatrix>
+    public class BitMatrix : IEquatable<BitMatrix>, IEnumerable<int>, ICloneable
     {
         private BitArray data;
         public int NumberOfRows { get; }
@@ -95,7 +95,9 @@ namespace BitMatrixLib
         }
         public bool Equals(BitMatrix other)
         {
-            if(data.Length != other.data.Length) return false;
+            if (ReferenceEquals(other,null)) return false;
+            if (NumberOfColumns != other.NumberOfColumns || NumberOfRows != other.NumberOfRows) return false;
+            if (data.Length != other.data.Length) return false;
             for(int i = 0; i < data.Length; i++)
             {
                 if (data[i] != other.data[i]) return false;
@@ -103,14 +105,25 @@ namespace BitMatrixLib
             return true;
         }
 
-        public bool Equals(object? other)
+        public override bool Equals(object other)
         {
-            BitMatrix b = (BitMatrix)other;
-            if (b == null) return false;
+            if (ReferenceEquals(other, null)) return false;
+            BitMatrix b;
+            try
+            {
+                b = (BitMatrix)other;
+            }
+            catch
+            {
+                return false;
+            }
             return data.Equals(b);
         }
 
         public static bool Equals(BitMatrix b1, BitMatrix b2) {
+            if (ReferenceEquals(b1, null) && ReferenceEquals(b2, null)) return true;
+            if (ReferenceEquals(b1, null) || ReferenceEquals(b2, null)) return false;
+            if (b1.NumberOfColumns != b2.NumberOfColumns || b1.NumberOfRows != b2.NumberOfRows) return false;
             if (b1.data.Length != b2.data.Length) return false;
             for (int i = 0; i < b1.data.Length; i++)
             {
@@ -119,5 +132,148 @@ namespace BitMatrixLib
             return true;
         }
 
+        public static bool operator==(BitMatrix b1, BitMatrix b2)
+        {
+            return Equals(b1, b2);
+        }
+        public static bool operator !=(BitMatrix b1, BitMatrix b2)
+        {
+            return !Equals(b1, b2);
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                int idx = i * NumberOfColumns + j;
+                if (i < 0 || j < 0 || i >= NumberOfRows || j >= NumberOfColumns) throw new IndexOutOfRangeException();
+                return BoolToBit(data[idx]);
+            }
+            set
+            {
+                int idx = i * NumberOfColumns + j;
+                if (i < 0 || j < 0 || i >= NumberOfRows || j >= NumberOfColumns) throw new IndexOutOfRangeException();
+                data[idx] = BitToBool(value);
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int i = 0; i < data.Length; i++)
+            {
+                yield return BoolToBit(this.data[i]);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public object Clone()
+        {
+            int[] newData = new int[data.Length];
+            data.CopyTo(newData, 0);
+            return new BitMatrix(this.NumberOfRows, this.NumberOfColumns, newData);
+        }
+
+        public static BitMatrix Parse(string s)
+        {
+            if (s == null || s == String.Empty) throw new ArgumentNullException();
+            string[] data = s.Split(Environment.NewLine);
+            int rows = data.Length;
+            int cols = data[0].Length;
+            bool[,] boolData = new bool[rows,cols];
+            int idx = 0;
+            for(int i = 0; i < rows; i++)
+            {
+                int j = 0;
+                for(j = 0; j < data[i].Length; j++)
+                {
+                    if (cols <= j) throw new FormatException();
+                    var tmp = data[i][j] - '0';
+                    if (tmp != 0 && tmp != 1) throw new FormatException();
+                    boolData[idx, j] = BitToBool(tmp);
+                }
+                if(cols != j) throw new FormatException();
+                idx++;
+            }
+
+            return new BitMatrix(boolData);
+        }
+
+        public static bool TryParse(string s, out BitMatrix result)
+        {
+            result = null;
+            if (s == null || s == String.Empty) return false;
+            string[] data = s.Split(Environment.NewLine);
+            int rows = data.Length;
+            int cols = data[0].Length;
+            bool[,] boolData = new bool[rows, cols];
+            int idx = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                int j = 0;
+                for (j = 0; j < data[i].Length; j++)
+                {
+                    if (cols <= j) return false;
+                    var tmp = data[i][j] - '0';
+                    if (tmp != 0 && tmp != 1) return false;
+                    boolData[idx, j] = BitToBool(tmp);
+                }
+                if (cols != j) return false;
+                idx++;
+            }
+            result = new BitMatrix(boolData);
+            return true;
+        }
+
+        public static explicit operator BitMatrix(int[,] input)
+        {
+            if (input == null) throw new NullReferenceException();
+            if (input.Length == 0) throw new ArgumentOutOfRangeException(); 
+            return new BitMatrix(input);
+        }
+
+        public static implicit operator int[,](BitMatrix input)
+        {
+            if (ReferenceEquals(input, null)) throw new NullReferenceException();
+            int[,] res = new int[input.NumberOfRows, input.NumberOfColumns];
+            for(int i = 0; i < res.GetLength(0); i++)
+            {
+                for(int j = 0; j < res.GetLength(1); j++)
+                {
+                    res[i, j] = input[i, j];
+                }
+            }
+            return res;
+        }
+
+        public static explicit operator BitMatrix(bool[,] input)
+        {
+            if (ReferenceEquals(input, null)) throw new NullReferenceException();
+            if (input.Length == 0) throw new ArgumentOutOfRangeException();
+            return new BitMatrix(input);
+        }
+
+        public static implicit operator bool[,](BitMatrix input)
+        {
+            if (ReferenceEquals(input, null)) throw new NullReferenceException();
+            bool[,] res = new bool[input.NumberOfRows, input.NumberOfColumns];
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    res[i, j] = BitToBool(input[i, j]);
+                }
+            }
+            return res;
+        }
+
+        public static explicit operator BitArray(BitMatrix input)
+        {
+            if (ReferenceEquals(input, null)) throw new NullReferenceException();
+            return new BitArray(input.data);
+        }
     }
 }
--- BitMatrix/ConsoleApp1/Program.cs
+++ BitMatrix/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BitMatrixLib;
+using System.Collections;
 
 namespace ConsoleApp1
 {
@@ -23,9 +24,90 @@ namespace ConsoleApp1
             BitMatrix b4 = new(bits2);
             BitMatrix b5 = new(bits2);
             Console.WriteLine(b3);
-            Console.WriteLine(b4 == b5);
+            // operator `==`, `!=`
+            // dwa `null`
+            BitMatrix m1 = null;
+            BitMatrix m2 = null;
+            Console.WriteLine(m1 == m2);
+            Console.WriteLine(m1 != m2);
 
 
+            // indekser - indeksy poza zakresem
+            int[] arr = new int[] { -1, 1, 3, 4 };
+            foreach (var i in arr)
+                foreach (var j in arr)
+                {
+                    var m = new BitMatrix(3, 4);
+                    try
+                    {
+                        m[i, j] = 1;
+                        Console.WriteLine($"m[{i}, {j}] = {m[i, j]}");
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine($"m[{i}, {j}] = exception");
+                    }
+                }
+
+            // sprawdzenie, czy tworzona
+            // jest niezależna kopia
+            var am = new BitMatrix(2, 3);
+            var am1 = (BitMatrix)(am.Clone());
+            am[0, 0] = 1;
+            Console.WriteLine(am[0, 0] != am1[0, 0]);
+            string s = @"1111
+0000
+1100";
+            BitMatrix sm = BitMatrix.Parse(s);
+            Console.WriteLine(sm.NumberOfRows);
+            Console.WriteLine(sm.NumberOfColumns);
+            Console.WriteLine(sm);
+
+            // konwersja jawna z `int[,]` na `BitMatrix`
+            // dane poprawne
+            string s2 = @"01
+10";
+            int[,] a = new int[,] { { 0, 1 }, { 1, 0 } };
+            var cm = (BitMatrix)a;
+            Console.WriteLine(2 == cm.NumberOfRows);
+            Console.WriteLine(2 == cm.NumberOfColumns);
+            var test = cm.ToString();
+            Console.WriteLine(s2 == test);
+
+            int[,] test2 = cm;
+            Console.WriteLine(test2);
+            bool[,] bool_test = new bool[,] { { false, true }, { true, false } };
+            var bool_test_matrix = (BitMatrix)bool_test;
+            bool[,] testowe = bool_test_matrix;
+            Console.WriteLine(testowe[0,1] == bool_test[0,1]);
+
+            // konwersja `BitMatrix` na `BitArray`
+            var c1m = new BitMatrix(2, 3, 1, 0, 1, 1, 1, 0);
+            BitArray bitArr = (BitArray)c1m;
+
+            Console.WriteLine(c1m.NumberOfRows * c1m.NumberOfColumns == bitArr.Count);
+            //Console.WriteLine(c1m);
+            //foreach(var bit in bitArr)
+            //{
+            //    Console.WriteLine(bit.ToString());
+            //}
+            //Console.WriteLine(bitArr);
+
+            for (int i = 0; i < c1m.NumberOfRows; i++)
+                for (int j = 0; j < c1m.NumberOfColumns; j++)
+                {
+                    Console.WriteLine($"TESTUJE: {c1m[i, j]} == {bitArr[i * c1m.NumberOfColumns + j]}");
+                    if (c1m[i, j] != BitMatrix.BoolToBit(bitArr[i * c1m.NumberOfColumns + j]))
+                    {
+                        Console.WriteLine("Fail");
+                        return;
+                    }
+                }
+
+            // czy niezależna kopia
+            c1m[1, 2] = 1;
+            
+            Console.WriteLine(c1m[1, 2] != BitMatrix.BoolToBit(bitArr[5]));
 
             Console.ReadLine();
         }
