4b49f0affd3761158ef0b3880f5957be5e677565
owr <proroog@gmail.com>
2025-04-24 11:23:21 +0200

    ChmodConverter init
    + ConsoleApp
    + AppLib
    + WPF App

---
+++ WPF-chmod-konwerter/ChmodConverterLib/Class1.cs
@@ -0,0 +1,82 @@
+﻿
+namespace ChmodConverterLib
+{
+    public class ChmodConverter
+    {
+        public static int SymbolicToNumeric(string input)
+        {
+            int res = 0;
+            int count = 0;
+            foreach (char c in input)
+            {
+                if(count >= 3)
+                {
+                    res *= 10;
+                    count = 0;
+                }
+                switch (c)
+                {
+                    case 'r':
+                        res += 4;
+                        break;
+                    case 'w':
+                        res += 2;
+                        break;
+                    case 'x':
+                        res += 1;
+                        break;
+                    case '-':
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid syntax!");
+                }
+                count++;
+            }
+            return res;
+        }
+
+        public static string NumericToSymbolic(int input)
+        {
+            int temp = input;
+            List<int> ints = new List<int>();
+            string res = "";
+            // 7 7 7
+            while(temp > 0)
+            {
+                ints.Add(temp % 10);
+                temp /= 10;
+            }
+            ints.Reverse();
+            for(int i = 0; i < ints.Count; i++)
+            {
+                if (ints[i] >= 4)
+                {
+                    res += "r";
+                    ints[i] -= 4;
+                }
+                else if (ints[i] >= 2 && ints[i] < 4)
+                {
+                    res += "w";
+                    ints[i] -= 2;
+                }
+                else if (ints[i] == 1)
+                {
+                    res += "x";
+                    ints[i] -= 1;
+                }
+                else if(ints[i] == 0)
+                {
+                    res += "-";
+                }
+
+                if (ints[i] > 0)
+                {
+                    i -= 1;
+                }
+            }
+
+            return res;
+        }
+    }
+}
+//rwxrwxrwx
---
+++ WPF-chmod-konwerter/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChmodConverterLib;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "rwxrwxrwx";
+            int tmp = ChmodConverter.SymbolicToNumeric(test);
+            Console.WriteLine(Convert.ToString(tmp));
+
+            int tmp2 = 777;
+            Console.WriteLine(ChmodConverter.NumericToSymbolic(tmp2));
+        }
+    }
+}
