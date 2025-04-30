8db09c7a202a8d324b41bd1f1bce8da4ba048f73
Prorog <proroog@gmail.com>
2025-04-30 20:59:40 +0200

    WPF-chmod-konwerter updates
    [+] MVC WebApp
    
    still need to check converter lib asap

---
+++ WPF-chmod-konwerter/WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using ChmodConverterLib;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string conversion_type, string conversion_data)
+        {
+            if(conversion_data != null && conversion_data.Length > 0)
+            {
+                string data = "Coœ posz³o nie tak!";
+                try
+                {
+                    if (conversion_type == "Symbolic")
+                    {
+                        data = ChmodConverter.NumericToSymbolic(Convert.ToInt32(conversion_data));
+                    }
+                    else
+                    {
+                        data = ChmodConverter.SymbolicToNumeric(conversion_data).ToString();
+                    }
+                    ViewBag.FormOverwriteData = $"{conversion_type}|{conversion_data}";
+                    var typ = conversion_type == "Symbolic" ? "symboliczny" : "numeryczny";
+                    ViewBag.FormData = $"{conversion_data} na typ {typ} => {data}";
+                }
+                catch (ArgumentException ex)
+                {
+                    ViewBag.FormData = $"{ex.Message}";
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
