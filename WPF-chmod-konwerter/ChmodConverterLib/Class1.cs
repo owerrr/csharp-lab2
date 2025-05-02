5b76553fc1e49aba5399eb87993b57f61fa582fa
Prorog <proroog@gmail.com>
2025-05-02 14:57:47 +0200

    WPF-chmod-konwerter updates
    changed webapp layout
    fixed chmod converter class
    fixed wpf layout

--- WPF-chmod-konwerter/ChmodConverterLib/Class1.cs
+++ WPF-chmod-konwerter/ChmodConverterLib/Class1.cs
@@ -5,7 +5,8 @@ namespace ChmodConverterLib
     {
         public static int SymbolicToNumeric(string input)
         {
-            if (input.Length % 3 != 0) throw new ArgumentException("Invalid syntax!");
+            int len = input.Length;
+            if (len != 9) throw new ArgumentException($"Invalid syntax! You typed {len} symbols! Valid pattern is 9 symbols length!");
             int res = 0;
             int count = 0;
             foreach (char c in input)
@@ -18,18 +19,21 @@ namespace ChmodConverterLib
                 switch (c)
                 {
                     case 'r':
+                        if (count != 0) throw new ArgumentException("Invalid syntax! letter 'r' is not in valid place!");
                         res += 4;
                         break;
                     case 'w':
+                        if (count != 1) throw new ArgumentException("Invalid syntax! letter 'w' is not in valid place!");
                         res += 2;
                         break;
                     case 'x':
+                        if (count != 2) throw new ArgumentException("Invalid syntax! letter 'x' is not in valid place!");
                         res += 1;
                         break;
                     case '-':
                         break;
                     default:
-                        throw new ArgumentException("Invalid syntax!");
+                        throw new ArgumentException($"Invalid syntax! Symbol {c} is not valid! Valid symbols: r, w, x, -");
                 }
                 count++;
             }
@@ -39,37 +43,48 @@ namespace ChmodConverterLib
         public static string NumericToSymbolic(int input)
         {
             int temp = input;
-            List<int> ints = new List<int>();
+            List<int> nums = new List<int>();
             string res = "";
             // 7 7 7
             while(temp > 0)
             {
                 int num = temp % 10;
-                if (num > 7 || num < 0) throw new ArgumentException("Invalid syntax!");
-                ints.Add(num);
+                if (num > 7 || num < 0) throw new ArgumentException($"Invalid syntax! Number {num} is out of range!");
+                nums.Add(num);
                 temp /= 10;
             }
-            ints.Reverse();
-            for(int i = 0; i < ints.Count; i++)
+            if (nums.Count != 3) throw new ArgumentException($"Invalid syntax! Your input is {nums.Count} length! Valid length is 3!");
+            nums.Reverse();
+            foreach(int n in nums)
             {
-                if (ints[i] >= 4)
+                switch (n)
                 {
-                    res += "r";
-                    ints[i] -= 4;
-                }
-                if (ints[i] >= 2)
-                {
-                    res += "w";
-                    ints[i] -= 2;
-                }
-                if (ints[i] == 1)
-                {
-                    res += "x";
-                    ints[i] -= 1;
-                }
-                else
-                {
-                    res += "-";
+                    case 7:
+                        res += "rwx";
+                        break;
+                    case 6:
+                        res += "rw-";
+                        break;
+                    case 5:
+                        res += "r-x";
+                        break;
+                    case 4:
+                        res += "r--";
+                        break;
+                    case 3:
+                        res += "-wx";
+                        break;
+                    case 2:
+                        res += "-w-";
+                        break;
+                    case 1:
+                        res += "--x";
+                        break;
+                    case 0:
+                        res += "---";
+                        break;
+                    default:
+                        throw new ArgumentException($"Invalid syntax! Argument {n} out of range!");
                 }
             }
 
